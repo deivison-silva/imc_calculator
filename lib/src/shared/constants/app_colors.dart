d0eade8ee2563d041b4cac4b1069273245e0a330
Deivison Silva <deivisondamiao01@outlook.com>
2021-08-24 23:54:40 -0300

    adds text and colors constants

---
+++ lib/src/shared/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color grey = Colors.grey.shade300;
+  static final Color white = Colors.white;
+  static final Color black = Colors.black;
+  static final Color green = Colors.green;
+  static final Color purpleAccent = Colors.purpleAccent;
+}
