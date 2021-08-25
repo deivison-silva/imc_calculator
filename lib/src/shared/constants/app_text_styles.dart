0ae00b363e77c8329dc8721e2273ffb6a5b7503f
Deivison Silva <deivisondamiao01@outlook.com>
2021-08-25 14:39:28 -0300

    adds minor changes to text styles and colors app and also export file

--- lib/src/shared/constants/app_text_styles.dart
+++ lib/src/shared/constants/app_text_styles.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import './app_colors.dart';
 
 class TextStyles {
-  static final TextStyle green42 = TextStyle(
-    fontSize: 42.0,
-    color: AppColors.green,
-  );
-  static final TextStyle green20 = TextStyle(
-    fontSize: 20.0,
-    color: AppColors.green,
-  );
   static final TextStyle white20 = TextStyle(
     fontSize: 20.0,
     color: AppColors.white,
   );
+
   static final TextStyle black16 = TextStyle(
     fontSize: 16.0,
+    color: AppColors.black.withOpacity(0.54),
+  );
+
+  static final TextStyle black20Italic = TextStyle(
+    fontSize: 20.0,
+    fontStyle: FontStyle.italic,
     color: AppColors.black.withOpacity(0.54),
   );
 }
---
+++ lib/src/shared/exports.dart
@@ -0,0 +1,4 @@
+export 'package:imc_calculator/src/shared/constants/app_text_styles.dart';
+export 'package:imc_calculator/src/shared/constants/app_colors.dart';
+export 'package:imc_calculator/src/shared/widgets/custom_button.dart';
+export 'package:imc_calculator/src/shared/widgets/custom_text_field.dart';
