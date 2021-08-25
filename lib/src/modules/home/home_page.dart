900ece4c5ae0cb78e4a648ae962066e37a4b6212
Deivison Silva <deivisondamiao01@outlook.com>
2021-08-25 14:43:20 -0300

    finishes ui home with state management

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../shared/constants/app_colors.dart';
-import '../../shared/constants/app_text_styles.dart';
-import '../../shared/widgets/custom_text_field.dart';
+import '../../shared/exports.dart';
+import './home_controller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,63 +10,102 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.grey,
       body: SingleChildScrollView(
         child: Container(
+          width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                width: 300.0,
-                height: 300.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(150.0),
-                  border: Border.all(width: 10, color: AppColors.green),
-                ),
-                child: Column(
+              controller.imc == 0
+                  ? Text(
+                      'Adicione valores de peso e altura\n para calcular o seu IMC',
+                      style: TextStyles.black20Italic,
+                      textAlign: TextAlign.center,
+                    )
+                  : Container(
+                      width: 300.0,
+                      height: 300.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(150.0),
+                        border: Border.all(
+                          width: 10,
+                          color: controller.getColor(controller.imc),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '${controller.imc.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontSize: 50.0,
+                              color: controller.getColor(controller.imc),
+                            ),
+                          ),
+                          Text(
+                            '${controller.getClassification(controller.imc)}',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              color: controller.getColor(controller.imc),
+                            ),
+                          ),
+                          SizedBox(height: 24.0),
+                          IconButton(
+                            icon: Icon(
+                              Icons.refresh_rounded,
+                              color: controller.getColor(controller.imc),
+                            ),
+                            onPressed: () => setState(() => controller.imc = 0),
+                          ),
+                        ],
+                      ),
+                    ),
+              SizedBox(height: 32.0),
+              Visibility(
+                visible: controller.imc == 0,
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('24.22', style: TextStyles.green42),
-                    Text('Peso normal', style: TextStyles.green20),
+                    Column(
+                      children: [
+                        Text('Seu peso', style: TextStyles.black16),
+                        SizedBox(height: 8.0),
+                        CustomTextField(
+                          controller: controller.weightController,
+                          action: TextInputAction.next,
+                          suffixText: 'kg',
+                          maxLength: 3,
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 24.0),
+                    Column(
+                      children: [
+                        Text('Sua altura', style: TextStyles.black16),
+                        SizedBox(height: 8.0),
+                        CustomTextField(
+                          controller: controller.heightController,
+                          action: TextInputAction.done,
+                          suffixText: 'm',
+                          maxLength: 4,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
-              SizedBox(height: 32.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Text('Seu peso', style: TextStyles.black16),
-                      SizedBox(height: 8.0),
-                      CustomTextField(suffixText: 'kg', maxLength: 2),
-                    ],
-                  ),
-                  SizedBox(width: 24.0),
-                  Column(
-                    children: [
-                      Text('Sua altura', style: TextStyles.black16),
-                      SizedBox(height: 8.0),
-                      CustomTextField(suffixText: 'm', maxLength: 3),
-                    ],
-                  ),
-                ],
-              ),
               SizedBox(height: 16.0),
-              ElevatedButton(
-                child: Text('Calcular', style: TextStyles.white20),
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(150.0, 50.0),
-                  primary: AppColors.purpleAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
+              Visibility(
+                visible: controller.imc == 0,
+                child: CustomButton(
+                  onPressed: () => setState(() => controller.calculate()),
                 ),
               ),
             ],
